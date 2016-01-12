b2ad1871e6b937e42fa2e56b8c0ed6b32193899a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-12 16:57:10 +0300

    Set up babel to write code in  es2015 style

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,13 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: [path.resolve('./', 'lib'), path.resolve('./', 'test')],
+                loader: "babel-loader"}
+        ]
     }
 };
