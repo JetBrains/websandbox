e1a08b8b417c3f830627b601f488b7ee2ea43dba
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-18 16:44:06 +0300

    Improve example

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -2,7 +2,7 @@ import Sandbox from '../../lib/sandbox-api';
 
 var localApi = {
     testApiFn: function (message) {
-        console.info('Host function called from iframe with: ' + message);
+        console.log('Host function called from iframe with: ' + message);
     }
 };
 
@@ -12,20 +12,22 @@ sandbox.promise
         console.log('Sandbox is created. Trying to run code inside');
 
         sandbox.runCode(`
-            console.log("Sandboxed code initialized successfully");
+            console.info("Sandboxed code initialized successfully");
             document.body.innerHTML = "Content is generated from the iframe";
             Sandboxjs.connection.remote.testApiFn("some argument");
 
             Sandboxjs.connection.setLocalApi({
                 sandboxedMethod: function(message) {
-                    console.info('sandboxed method called successfully:', message);
+                    console.info('sandboxedMethod called successfully:', message);
+                    return 'this is sandboxedMethod result';
                 }
             });
         `)
             .then(() => console.log('Code has been ran'))
             .then(() => {
-                console.log('Calling sandboxed method...');
-                sandbox.connection.remote.sandboxedMethod('hello from host');
+                console.log('Calling sandboxedMethod...');
+                sandbox.connection.remote.sandboxedMethod('hello from host')
+                    .then(res => console.log('Call was successful:', res));
             });
     });
 
