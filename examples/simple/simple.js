e354d2e707edc40d7ac5109b8c2d7b036f1666d8
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-18 16:39:27 +0300

    Add calling back to example

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -2,20 +2,30 @@ import Sandbox from '../../lib/sandbox-api';
 
 var localApi = {
     testApiFn: function (message) {
-        console.log('Host function called from iframe with: ' + message);
+        console.info('Host function called from iframe with: ' + message);
     }
 };
 
 const sandbox = Sandbox.create(localApi, {iframeContainer: '.iframe__container', frameClassName: 'simple__iframe'});
 sandbox.promise
     .then(() => {
-        console.log('sandbox is created!', sandbox);
+        console.log('Sandbox is created. Trying to run code inside');
 
         sandbox.runCode(`
-            console.info("Logging from iframe is working.");
+            console.log("Sandboxed code initialized successfully");
             document.body.innerHTML = "Content is generated from the iframe";
             Sandboxjs.connection.remote.testApiFn("some argument");
+
+            Sandboxjs.connection.setLocalApi({
+                sandboxedMethod: function(message) {
+                    console.info('sandboxed method called successfully:', message);
+                }
+            });
         `)
-            .then(() => console.log('Code has been ran'));
+            .then(() => console.log('Code has been ran'))
+            .then(() => {
+                console.log('Calling sandboxed method...');
+                sandbox.connection.remote.sandboxedMethod('hello from host');
+            });
     });
 
