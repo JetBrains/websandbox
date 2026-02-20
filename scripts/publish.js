b441db6cfccae4bb6c73396a96e5e2651eea3d1b
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2026-02-20 16:27:14 +0100

    Fix gh-pages deploy authentication in CI
    Pass GITHUB_TOKEN to the deploy step and use a token-authenticated
    repo URL in publish.js so gh-pages can push to the gh-pages branch.

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,9 +3,20 @@
 // eslint-disable-next-line no-undef
 const ghpages = require('gh-pages');
 
+const options = {branch: 'gh-pages'};
+
+// Use token-authenticated URL in CI
+// eslint-disable-next-line no-undef
+if (process.env.GITHUB_TOKEN) {
+  // eslint-disable-next-line no-undef
+  const repo = process.env.GITHUB_REPOSITORY || 'JetBrains/websandbox';
+  // eslint-disable-next-line no-undef
+  options.repo = `https://git:${process.env.GITHUB_TOKEN}@github.com/${repo}.git`;
+}
+
 console.log('publishing...');
 
-ghpages.publish('dist-examples', {branch: 'gh-pages'}, function(err) {
+ghpages.publish('dist-examples', options, function(err) {
   if (err) {
     console.error('failed to publish', err);
   } else {
